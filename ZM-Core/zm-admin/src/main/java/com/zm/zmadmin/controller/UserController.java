d5fddb522d35e9e1eccb31f5f90daf811ed74665
易作敏 <278406977@qq.com>
2020-04-26 15:15:13 +0800

    部分修改

---
+++ ZM-Core/zm-admin/src/main/java/com/zm/zmadmin/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.zm.zmadmin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ==================================
+ *
+ * @author yizuomin
+ * @description:
+ * @date 2020/4/26 15:14
+ * ==================================
+ **/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
